5294af577b9c533fcc73020eee3782cda4e1ddb5
Герман <108299345+german-spb@users.noreply.github.com>
2023-01-03 15:06:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,19 +36,22 @@ def add_phone(cursor, person_id, number_phone):
     INSERT INTO phone(person_id, number_phone) VALUES(%s, %s);
     """, (person_id, number_phone))
 
-def change_client(cursor, id, first_name: str =None, last_name :str=None, email:str=None, number_phone=None):
-    cursor.execute("""
-    UPDATE person
-    SET first_name = %s WHERE id = %s;
-    """, (first_name, id))
-    cursor.execute("""
-    UPDATE person
-    SET last_name = %s WHERE id = %s;
-    """, (last_name, id))
-    cursor.execute("""
-    UPDATE person
-    SET email = %s WHERE id = %s;
-    """, (email, id))
+def change_client(cursor, id, first_name:str=None, last_name:str=None, email:str=None, number_phone=None):
+    if first_name is not None:
+     cursor.execute("""
+     UPDATE person
+     SET first_name = %s WHERE id = %s;
+     """, (first_name, id))
+    if last_name is not None:
+     cursor.execute("""
+     UPDATE person
+     SET last_name = %s WHERE id = %s;
+     """, (last_name, id))
+    if email is not None:
+     cursor.execute("""
+     UPDATE person
+     SET email = %s WHERE id = %s;
+     """, (email, id))
 
 def delete_phone(cursor, id, number_phone):
     cursor.execute("""
@@ -67,14 +70,34 @@ def delete_client(cursor, id):
         """, (id,))
 
 def find_client(cursor, first_name: str =None, last_name :str=None, email:str=None, number_phone=None):
-    cursor.execute("""
-    SELECT first_name, last_name, email, number_phone FROM person
-    LEFT JOIN phone ON person.id = phone.person_id
-    WHERE first_name = %s OR last_name = %s OR email = %s OR number_phone = %s;
-    """, (first_name, last_name, email, number_phone))
+    if number_phone is not None:
+     cursor.execute("""
+     SELECT first_name, last_name, email, number_phone FROM person
+     LEFT JOIN phone ON person.id = phone.person_id
+     WHERE number_phone = %s;
+     """, (number_phone,))
+    if first_name is not None:
+     cursor.execute("""
+     SELECT first_name, last_name, email, number_phone FROM person
+     LEFT JOIN phone ON person.id = phone.person_id
+     WHERE first_name = %s;
+     """, (first_name,))
+    if last_name is not None:
+     cursor.execute("""
+     SELECT first_name, last_name, email, number_phone FROM person
+     LEFT JOIN phone ON person.id = phone.person_id
+     WHERE last_name = %s;
+     """, (last_name,))
+    if email is not None:
+     cursor.execute("""
+     SELECT first_name, last_name, email, number_phone FROM person
+     LEFT JOIN phone ON person.id = phone.person_id
+     WHERE email = %s;
+     """, (email,))
 
 
-with psycopg2.connect(database="clients_db", user="german", password="gkoretskiy27") as conn:
+if __name__ == '__main__':
+  with psycopg2.connect(database="clients_db", user="german", password="gkoretskiy27") as conn:
     with conn.cursor() as cur:
         # cur.execute("""
         # DROP TABLE phone;
@@ -83,18 +106,18 @@ with psycopg2.connect(database="clients_db", user="german", password="gkoretskiy
         # --- создание БД
         # create_db(cur)
         # --- добавление нового клиента
-        # add_client(cur, 'Ирина', 'Кожина',  'irina@pushkin-spb.com', 9112240404)
+        # add_client(cur, 'Марк', 'Корецкий',  'mark@pushkin-spb.com', 9111764586)
         # --- добавление телефона
-        # add_phone(cur, 1, 9112240505)
+        # add_phone(cur, 1, 9213504883)
         # --- изменение данных
-        # change_client(cur, 2, first_name='Ирина', last_name='Корецкая', email='irina@pushkin-spb.com')
+        # change_client(cur, 1, email = 'irina@pushkin-spb.com')
         # --- удаление телефона
         # delete_phone(cur, 1, 9112240505)
         # --- удаление клиента
         # delete_client(cur, 1)
         # --- поиск клиента
-        # find_client(cur, number_phone='9112240404')
-        # print(cur.fetchone())
+        find_client(cur, first_name='Герман', last_name='Корецкий', number_phone=9042240404)
+        print(cur.fetchone())
 
 
         cur.execute("""
